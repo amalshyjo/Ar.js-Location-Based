c54c2c595b39062529f2bba5ff9977f5e5821940
amalshyjo <56060390+amalshyjo@users.noreply.github.com>
2020-12-16 18:41:40 +0530

    V2 location change

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 9.955460,
+                lng: 76.651110,
             },
         },
     ];

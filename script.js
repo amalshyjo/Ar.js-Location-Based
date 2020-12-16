ad79ecb465bf8e52c1fc5bdf08a8930b5da5b9ad
amalshyjo <56060390+amalshyjo@users.noreply.github.com>
2020-12-16 19:14:58 +0530

    LOC CHECK

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 9.981636,
+                lng: 76.299881,
             },
         },
     ];
